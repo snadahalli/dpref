ad3674d22d3f0c341cd483420ba0dfe4f67f6c72
dimmik.a <dimmik.a@c047af9a-d421-11de-9f39-755c91dee682>
2009-11-20 15:11:06 +0000

    warning fixed

--- trunk/Preferance/test/com/dimmik/cards/pref/PrefDealTest.java
+++ trunk/Preferance/test/com/dimmik/cards/pref/PrefDealTest.java
@@ -14,6 +14,7 @@ import com.dimmik.cards.table.Seat;
 import junit.framework.TestCase;
 
 public class PrefDealTest extends TestCase {
+    @SuppressWarnings("serial")
     public void testDeal() throws Throwable{
 	List<Seat> seats = new ArrayList<Seat>(){{
 		   add(new Seat("West")); 
